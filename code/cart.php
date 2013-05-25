e962c408e718297c5feabd17cdbd9f7dc587fae3
Rashmi Nagaraju <rashminagaraju@Rashmi-Nagarajus-MacBook-Pro.local>
2013-05-24 17:41:45 -0700

    modified to use prepared statements(Priya)

--- code/cart.php
+++ code/cart.php
@@ -10,11 +10,16 @@
 
 		<?php
 			$user_id = $_SESSION['cuserid'];
-			$result = mysqli_query($con,"SELECT title,isbn,coverpage FROM book where isbn in (select book_isbn from cart where user_id = $user_id)");
-			
-			if($result->num_rows>0)
-			{
-			
+			// $result = mysqli_query($con,"SELECT title,isbn,coverpage FROM book where isbn in (select book_isbn from cart where user_id = $user_id)");
+// 			
+// 			if($result->num_rows>0)
+// 			{
+		$stmt=$con->prepare("select title, isbn, coverpage from book where isbn in (select book_isbn from cart where user_id=?)");
+		$stmt->bind_param("s",$user_id);
+		if($stmt->execute()){
+		$stmt->bind_result($title,$isbn,$coverpage);
+		echo $title;
+		
 		?>
 		<table border=1 class=Form>
 			<tr>
@@ -24,12 +29,17 @@
 			</tr>
 
 		<?php	
-			  while($row = mysqli_fetch_array($result)){ 
+			  while(mysqli_stmt_fetch($stmt)){ 
 		?>
 				<tr>
-					<td><?php echo '<img src="data:image/jpeg;base64,' . base64_encode( $row['coverpage'] ) . '" heigh="92" width="42"/>'; ?></td>
+					<!-- 
+<td><?php echo '<img src="data:image/jpeg;base64,' . base64_encode( $row['coverpage'] ) . '" heigh="92" width="42"/>'; ?></td>
 					<td><?php echo $row['title']; ?></td>
 					<td><a href="remove_cart.php?isbn=<?php echo $row['isbn']?>">Remove</a></td>
+ -->
+ <td><?php echo '<img src="data:image/jpeg;base64,' . base64_encode( $coverpage ) . '" heigh="92" width="42"/>'; ?></td>
+					<td><?php echo $title; ?></td>
+					<td><a href="remove_cart.php?isbn=<?php echo $isbn?>">Remove</a></td>
 				</tr>
 		<?php } ?>
 		
@@ -37,6 +47,7 @@
 		<a href="revalidate_login.php">Checkout</a>
 		<?php	
 		}
+		
 			else 
 			{
 					echo "Cart is Empty!!!...Add books to the cart";
