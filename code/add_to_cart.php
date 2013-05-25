cc466dceb278ca5db2feabe290ee086c674a7034
Priya <priyagarnepudi@gmail.com>
2013-05-24 22:30:45 -0700

    Added prepare statements

--- code/add_to_cart.php
+++ code/add_to_cart.php
@@ -5,13 +5,20 @@
 
 	$isbn = $_GET['isbn'];
 	$flag = 0;
-    
-	$result = mysqli_query($con,"SELECT isbn FROM book WHERE isbn = $isbn");
-	if($result){
-
-		if($result->num_rows == 1){
+    // 
+// 	$result = mysqli_query($con,"SELECT isbn FROM book WHERE isbn = $isbn");
+// 	if($result){
+		$stmt=$con->prepare("select isbn from book where isbn=?");
+		$stmt->bind_param("s",$isbn);
+		if($stmt->execute()){
+		$row = $stmt->fetch();
+		
+//echo $row;
+		if($row == 1){
+//if($result->num_rows == 1){
 			$session_id = 123;
 			$user_id = $_SESSION['cuserid'];
+			
 /*
 			$stmt = $con->prepare("insert into cart(user_id, book_isbn, session_id) values (?,?,?)");
 
@@ -23,10 +30,17 @@
      		echo "success";
      		*/
 
-     		$result = mysqli_query($con,"insert into cart (user_id, book_isbn, session_id) values ('$user_id','$isbn','$session_id')");
-     		if(!$result){
-     			echo "Error adding to cart!" .mysqli_error($con);
-     		}	
+    // $ result = mysqli_query($con,"insert into cart (user_id, book_isbn, session_id) values ('$user_id','$isbn','$session_id')");
+//      		if(!$result){
+//      			echo "Error adding to cart!" .mysqli_error($con);
+//      		}	
+			$stmt->close();
+			$stmt1 = $con->prepare("insert into cart(user_id,book_isbn,session_id) values (?,?,?)");
+			$stmt1->bind_param("sss",$user_id,$isbn,$session_id);
+			if(!$stmt1->execute()){
+			echo "Error adding to cart!";
+			}
+		 //else{echo "Error adding to cart!";}
      		
      		header('Location:home.php');
 		}else{
