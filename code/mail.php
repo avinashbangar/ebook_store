4a35e053a9dbace1ceb1ef3edace764137861f00
Sergio Peleato <speleato@gmail.com>
2013-05-26 18:45:12 -0700

    mail modified

--- code/mail.php
+++ code/mail.php
@@ -2,24 +2,6 @@
 	require 'connect.php';
 	include_once 'Utility.php';
 	
-/* 	function GenerateRandomString()
-	{
-		$length = 15;
-		$randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
-		return $randomString;
-	}
-	
-	function GenerateHashedString($input)
-	{
-		//Generates the hashed value of a string and returns its value
-		//$6 --> Algorithm prefix
-		//$rounds --> nº of times the algorithm is going to be looping
-		//$SillyString -->string used for encryption.
-		$hashed = crypt($input,'$6$rounds=8000$Pikachu3Dabai4$');
-		
-		//this will return the generated key
-		return substr($hashed,30);
-	}*/
 	
 	function UpdatePassword($hashedValue, $address)
 	{
@@ -54,6 +36,7 @@
 		
 		//execute the query
 		 $stmt->execute();
+		 $stmt->store_result();
 		 if(1==$stmt->num_rows) {
 		 	
 			//There is a user with such an email, password and in the correct timestamp
