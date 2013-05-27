c285a4d97877d1dad6c3074b9860325042d900c5
Sergio Peleato <speleato@gmail.com>
2013-05-26 17:45:25 -0700

    Modifications on buy.php to generate the files correctly

--- code/buy.php
+++ code/buy.php
@@ -22,8 +22,8 @@
 		$isbn = $_POST['isbn'];
 		$ticket = GenerateRandomString();
 		
-		$stmt = $con->prepare("INSERT INTO `order` (user_id,book_isbn,hash_Ticket) VALUES (?,?,?)");
-		$stmt->bind_param("iis",$user_id,$isbn,hash('sha512',$ticket));
+		$stmt = $con->prepare("INSERT INTO `order` (user_id,book_isbn,hash_Ticket, downloaded) VALUES (?,?,?,?)");
+		$stmt->bind_param("iisi",$user_id,$isbn,hash('sha512',$ticket), intval(0));
 		if($stmt->execute()){
 			echo "<br/><a href='download.php?isbn=".$isbn."&ticket=".$ticket."' target='_blank'>Download Ebook!</a>";
 		}
